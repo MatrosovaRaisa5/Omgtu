249369e51388dbf8ac29eb123921620c2ec209ba
MatrosovaRaisa5 <minteee@list.ru>
2023-09-20 05:42:36 +0600

    Create Class-18-09-23(3).cs

---
+++ algorithm/labclass/Class-18-09-23(3).cs
@@ -0,0 +1,20 @@
+﻿using System;
+class HelloWorld
+{
+    static void Main()
+    {
+        int a, b;
+        int modul, min, max;
+        a = Convert.ToInt32(Console.ReadLine());
+        b = Convert.ToInt32(Console.ReadLine());
+        modul = Math.Abs(a - b);
+        min = (a - modul + b) / 2;
+        max = (a + modul + b) / 2;
+
+
+
+        Console.WriteLine(min);
+        Console.WriteLine(max);
+
+    }
+}
