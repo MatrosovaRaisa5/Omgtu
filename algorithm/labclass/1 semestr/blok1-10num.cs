981588e9d31c054fdefc01d014d99a66a50e9ae6
MatrosovaRaisa5 <minteee@list.ru>
2024-02-27 22:14:41 +0630

    Create blok1-10num.cs

---
+++ algorithm/labclass/1 semestr/blok1-10num.cs
@@ -0,0 +1,33 @@
+﻿using System;
+class HelloWorld
+{
+    static void Main()
+    {
+        int kolvootr = 0;
+        int kolvo3 = 0;
+        int summ35 = 0;
+        int prod = 1;
+        for (int i = 0; i < 10; i++)
+        {
+            int s;
+            s = Convert.ToInt32(Console.ReadLine());
+            if (s < 0)
+            { kolvootr += 1; }
+
+            if (Math.Abs(s) % 10 == 3)
+            { kolvo3 += 1; }
+
+            if (Math.Abs(s) % 3 == Math.Abs(s) % 5)
+            { summ35 += s; }
+
+            if (Math.Abs(s) % 2 == 0 && s >= 0)
+            { prod *= s; }
+        }
+        Console.WriteLine("Количество отрицательных чисел: " + kolvootr);
+        Console.WriteLine("Количество чисел, оканчивающихся на 3: " + kolvo3);
+        Console.WriteLine("Сумма чисел, которые в 3ичной и 5ичной системах оканчиваются одинаково: " + summ35);
+        if (prod == 1) { Console.WriteLine("Произведение четных неотрицательных чисел: 0"); }
+        else { Console.WriteLine("Произведение четных неотрицательных чисел: " + prod); }
+
+    }
+}
