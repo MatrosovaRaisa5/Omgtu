82be7ee6c7b7245416ed4b132f28d0cc891bfe08
MatrosovaRaisa5 <144814629+MatrosovaRaisa5@users.noreply.github.com>
2024-03-04 10:05:52 +0600

    Add files via upload

---
+++ discretka/AlgDijkstra.py
@@ -0,0 +1,57 @@
+def input_graph():
+    n = int(input("Введите количество вершин: "))
+    matrix = []
+
+    for i in range(n):
+        print(f"Введите строку {i + 1} матрицы смежности, разделенную пробелами (0 - если соединения нет):")
+        m = list(map(int, input().split()))
+        if len(m) != n:
+            print(f"Строка должна содержать {n} чисел. Попробуйте ввести строку {i + 1} снова.")
+            return input_graph()
+        matrix.append(m)
+
+    for i in range(n):
+        for j in range(n):
+            if i != j and matrix[i][j] == 0:
+                matrix[i][j] = float('infinity')
+
+    return n, matrix
+
+def dijkstra(n, matrix, start):
+    short = [float('infinity')] * n
+    short[start] = 0
+    visited = set()
+
+    while len(visited) < n:
+        min = None
+        for versh in range(n):
+            if versh  not in visited:
+                if min is None or short[versh] < short[min]:
+                    min = versh 
+
+        if min is None:
+            break  
+
+        visited.add(min)
+        for versh in range(n):
+            if matrix[min][versh] != float('infinity'):
+                distance = matrix[min][versh]
+                if short[min] + distance < short[versh]:
+                    short[versh] = short[min] + distance
+    
+    return short
+
+
+n, matrix = input_graph()
+start = int(input("Введите стартовую вершину (отсчет с 1): ")) - 1
+if start < 0 or start >= n:
+    print("Стартовая вершина вне допустимого диапазона. Попробуйте снова.")
+        
+
+short = dijkstra(n, matrix, start)
+    
+for versh, distance in enumerate(short):
+    if distance == float('infinity'):
+        print(f"Вершина {start + 1} не имеет пути к вершине {versh + 1}")
+    else:
+        print(f"Кратчайшее расстояние от вершины {start + 1} до вершины {versh + 1} равно {distance}")
