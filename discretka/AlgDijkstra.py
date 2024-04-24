29d724d8cdae1a54f385b05cd55eca2cd5d51dfc
Raisa <144814629+MatrosovaRaisa5@users.noreply.github.com>
2024-04-24 08:15:33 +0600

    Update AlgDijkstra.py

--- discretka/AlgDijkstra.py
+++ discretka/AlgDijkstra.py
@@ -21,7 +21,8 @@ def dijkstra(n, matrix, start):
     short = [float('infinity')] * n
     short[start] = 0
     visited = set()
-
+    parents = [-1] * n  
+  
     while len(visited) < n:
         min = None
         for versh in range(n):
@@ -38,20 +39,28 @@ def dijkstra(n, matrix, start):
                 distance = matrix[min][versh]
                 if short[min] + distance < short[versh]:
                     short[versh] = short[min] + distance
-    
-    return short
+                    parents[versh] = min 
+  
+    return short, parents
 
+def restore_path(parents, finish):
+    path = []
+    while finish != -1:
+        path.append(finish + 1) 
+        finish = parents[finish]
+    path.reverse()  
+    return path
 
 n, matrix = input_graph()
 start = int(input("Введите стартовую вершину (отсчет с 1): ")) - 1
 if start < 0 or start >= n:
     print("Стартовая вершина вне допустимого диапазона. Попробуйте снова.")
-        
-
-short = dijkstra(n, matrix, start)
+else:
+    short, parents = dijkstra(n, matrix, start)
     
-for versh, distance in enumerate(short):
-    if distance == float('infinity'):
-        print(f"Вершина {start + 1} не имеет пути к вершине {versh + 1}")
-    else:
-        print(f"Кратчайшее расстояние от вершины {start + 1} до вершины {versh + 1} равно {distance}")
+    for versh, distance in enumerate(short):
+        if distance == float('infinity'):
+            print(f"Вершина {start + 1} не имеет пути к вершине {versh + 1}")
+        else:
+            path = restore_path(parents, versh)
+            print(f"Кратчайшее расстояние от вершины {start + 1} до вершины {versh + 1} равно {distance}, Path: {' -> '.join(map(str, path))}")
